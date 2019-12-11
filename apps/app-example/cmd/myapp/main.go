369a14678c20f3015a022a631e42a177958fa6c5
Bruno Lucena <bvlg900f@gmail.com>
2019-12-11 11:23:55 +0100

    refactoring: setting the formater to logs be catched by fluentd

--- apps/app-example/cmd/myapp/main.go
+++ apps/app-example/cmd/myapp/main.go
@@ -13,11 +13,14 @@ var (
 )
 
 func main() {
-	// Config
+	// Log Config
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	// Default is stderr
+	logrus.SetOutput(os.Stdout)
 	// Will log anything that is info or above (warn, error, fatal, panic).
 	logrus.SetLevel(logrus.InfoLevel)
+	// App config
 	serverAddr := ":" + os.Getenv("API_CONTAINER_PORT")
-	// End
 	// Start App
 	r.StartWebServerHTTP(appName, serverAddr)
 }
