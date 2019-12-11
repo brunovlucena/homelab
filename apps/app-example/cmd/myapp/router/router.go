2f41d459d500b54c270330247b6a1d2ab1e2acc1
Bruno Lucena <bvlg900f@gmail.com>
2019-12-11 20:51:39 +0100

    removing extra logging

--- apps/app-example/cmd/myapp/router/router.go
+++ apps/app-example/cmd/myapp/router/router.go
@@ -171,9 +171,6 @@ func Create(w http.ResponseWriter, r *http.Request) {
 	err := cr.Bind(r)
 	// check error
 	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"cmd": "Bind",
-		}).Error(err.Error())
 		// return error
 		render.Render(w, r, ErrRender(err))
 		return
